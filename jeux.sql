d3439c0f850aab9609fddc7963d72ced07bbda77
hamed46 <47857182+hamed46@users.noreply.github.com>
2021-03-01 23:18:18 +0100

    Add files via upload

---
+++ jeux.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 01 mars 2021 à 23:15
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jeux`
+--
+
+CREATE TABLE `jeux` (
+  `id_client` int(11) NOT NULL,
+  `nom_client` int(20) NOT NULL,
+  `nom_jeu` varchar(20) NOT NULL,
+  `niv-diff` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `jeux`
+--
+ALTER TABLE `jeux`
+  ADD PRIMARY KEY (`id_client`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
