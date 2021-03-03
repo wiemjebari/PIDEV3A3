0880de7b515e8b6336184ede9a5b1a2683fe6e28
ichraf91 <79063202+ichraf91@users.noreply.github.com>
2021-03-03 23:16:34 +0100

    Add files via upload

---
+++ randonnees.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 03 mars 2021 à 23:15
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `randonnees`
+--
+
+CREATE TABLE `randonnees` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `lieu` varchar(255) NOT NULL,
+  `heure_depart` int(10) NOT NULL,
+  `heure_retour` int(10) NOT NULL,
+  `nbr_places` int(10) NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
